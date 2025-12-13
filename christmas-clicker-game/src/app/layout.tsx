3d5b0ba054c414a714b5edb2715d27aea2f1a550
Alexander <verycoolandsmart@protonmail.com>
2025-12-13 05:30:02 +0000

    second commit...........

---
+++ christmas-clicker-game/src/app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Cinzel, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const myFont = Cinzel({ // make sure to import from "next/font/google"!
+	subsets: ["latin"],
+	weight: "variable", // some fonts aren't variable, so you'll need to figure out what 
+  // weight to use. Google Fonts can tell you this, usually.
+	variable: "--font-cinzel", // for css
+})
+
+const geistSans = Cinzel({
+  variable: "--font-cinzel",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Haxmas Day 1",
+  description: "on the first day of haxmas my orpheus gave to me.....a react orpheus clicker game!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ christmas-clicker-game/src/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Image from "next/image";
+import JollyOrpheusClick from "@/components/JollyOrpheusClick";
+import {useState} from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0); // useState!
+  return (
+    <div className="min-h-screen bg-linear-to-br from-[#0f472a] to-[#1a5a3a] flex flex-col">
+      <main className="flex min-h-screen flex-col items-center justify-between py-32 px-16 bg-linear-to-br from-[#C8102E] to-[#215732]">
+        <div className="flex flex-col items-center gap-6 text-center">
+          <h1 className={"text-6xl self-center my-10"}>Haxmas Day 1</h1>
+          <p className={"text-3xl font-bold"}>times orpheus has been clicked: {count}</p>
+          <JollyOrpheusClick onClick={() => {setCount(count + 1)}} />
+            <p className={"text-3xl font-bold"}>click the orpheus to be awarded with a JOLLY GIFT.</p>
+            <a href="https://github.com/Snowflake6413">made by Snowflake6413s</a>
+            <img src="https://assets.hackclub.com/flag-orpheus-left.png"></img>
+            <a href="https://hackclub.com">click to join the hack club</a>
+        </div>
+      </main>
+    </div>
+  );
+}
